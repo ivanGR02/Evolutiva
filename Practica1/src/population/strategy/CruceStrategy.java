365b2229d2286d985ad98b492d46d42c35f7c385
ivanGR02 <125132574+ivanGR02@users.noreply.github.com>
2025-04-02 14:57:42 +0200

    Delete Practica1 directory

--- Practica1/src/population/strategy/CruceStrategy.java
+++
@@ -1,9 +0,0 @@
-package population.strategy;
-
-import java.util.ArrayList;
-
-import prac1.cromosoma.Cromosoma;
-
-public interface CruceStrategy {
-	void reproduccion(ArrayList<Cromosoma> pob,Double prob_cruse);
-}
--- Practica1/src/population/strategy/MutationStrategy.java
+++
@@ -1,9 +0,0 @@
-package population.strategy;
-
-import java.util.ArrayList;
-
-import prac1.cromosoma.Cromosoma;
-
-public interface MutationStrategy{
-	void mutacion(ArrayList<Cromosoma> pob,Double prob_mutation);
-}
